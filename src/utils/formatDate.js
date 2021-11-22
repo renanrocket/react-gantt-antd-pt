d76f47f15ee13c8b57f1f6cc28c86f450a9fa4a1
renanrocket <renan.the.rock@gmail.com>
2021-11-22 17:23:46 -0300

    tradução dos meses do ano do mes e do dia

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,5 @@
-const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const monthNames = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
 
 export const getMonth = date => monthNames[date.getMonth()]
 
-export const getDayMonth = date => `${date.getMonth() + 1}月${date.getDate()}日`
+export const getDayMonth = date => `${date.getMonth() + 1}mês ${date.getDate()}dia`
